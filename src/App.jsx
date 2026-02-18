df8817b12b02604619a2bf37a708e2619fec4b9d
Sarthak S <devilarena75@gmail.com>
2026-02-18 12:38:16 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
@@ -10,7 +10,7 @@ import Charts from './pages/Charts';
 function App() {
   return (
     <GlobalProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Dashboard />} />
@@ -19,7 +19,7 @@ function App() {
             <Route path="settings" element={<Settings />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </GlobalProvider>
   );
 }
